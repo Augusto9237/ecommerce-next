ab0c9ddd1e900ad393e3f0721c8e64b29abf8b88
Augusto9237 <79643279+Augusto9237@users.noreply.github.com>
2023-02-08 15:19:08 -0300

    1 file modified

--- codegen.ts
+++ codegen.ts
@@ -1,19 +1,18 @@
-
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://thekingtesters.saleor.cloud/graphql/",
-  documents: "src/**/.tsx",
+  schema: 'https://thekingtesters.saleor.cloud/graphql/',
+  documents: 'src/**/*.tsx',
   generates: {
-    "src/gql": {
-      preset: "client",
-      plugins: []
+    'src/gql': {
+      preset: 'client',
+      plugins: [],
     },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
-  }
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
 };
 
 export default config;
