456f929f0769e0a35442e0bb17884ab68e19b4f9
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-02-20 13:30:39 +0000

    ok

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header className="flex justify-center max-sm:p-2 p-8">
-      <div className="flex flex-1  max-sm:flex-col max-sm:gap-2 justify-between gap-6 items-center max-w-[850px]">
+      <div className="flex flex-1  max-sm:flex-col max-sm:gap-1 justify-between gap-6 items-center max-w-[850px]">
         <strong className="font-bold text-2xl leading-relaxed text-blue-600">
           Cosmo
         </strong>
