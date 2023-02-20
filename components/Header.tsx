a022a0c6a5fef4d4d28092a9c14e2e545302d221
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-02-20 13:30:08 +0000

    ok

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,37 +1,50 @@
 export function Header() {
   return (
-    <header className="flex justify-center p-8">
-    <div className="flex flex-1 justify-between items-center max-w-[850px]">
-      <strong className="font-bold text-2xl leading-relaxed text-blue-600">
-        Cosmo
-      </strong>
+    <header className="flex justify-center max-sm:p-2 p-8">
+      <div className="flex flex-1  max-sm:flex-col max-sm:gap-2 justify-between gap-6 items-center max-w-[850px]">
+        <strong className="font-bold text-2xl leading-relaxed text-blue-600">
+          Cosmo
+        </strong>
 
-      <div className="flex gap-6 items-center">
-        <span className="text-gray-700 text-lg ">
-          Sales
-        </span>
-        <span className="text-gray-700 text-lg">
-          Hoodie
-        </span>
-        <span className="text-gray-700 text-lg">
-          Fashion
-        </span>
-      
+        <div className="max-sm:hidden flex flex-1 gap-6 items-center">
+          <span className="text-gray-700 text-lg ">
+            Sales
+          </span>
+          <span className="text-gray-700 text-lg">
+            Hoodie
+          </span>
+          <span className="text-gray-700 text-lg">
+            Fashion
+          </span>
+        </div>
+
+        <div className="flex max-sm:gap-4 w-full">
+          <input type="text" placeholder="Search" className="flex flex-1 border border-gray-400 rounded-lg p-1" />
+
+          <button className="flex p-1 rounded-lg border border-gray-400 gap-1">
+            🛒 <span>0</span>
+          </button>
+
+          <div className="flex p-1 px-1.5 rounded-full bg-gray-300">
+            <span>
+              🙎‍♂️
+            </span>
+          </div>
+        </div>
+
+        <div className="max-sm:flex sm:hidden gap-6 items-center">
+          <span className="text-gray-700 text-lg ">
+            Sales
+          </span>
+          <span className="text-gray-700 text-lg">
+            Hoodie
+          </span>
+          <span className="text-gray-700 text-lg">
+            Fashion
+          </span>
+        </div>
+
       </div>
-   
-      <input type="text" placeholder="Search"  className="border border-gray-400 rounded-lg p-1"/>
-
-      <button className="flex p-1 rounded-lg border border-gray-400 gap-1">
-        🛒 <span>0</span>
-       </button>
-
-       <div className="flex p-1 px-1.5 rounded-full bg-gray-300">
-        <span>
-        🙎‍♂️
-        </span>
-       </div>
-
-       </div>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,16 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [],
 };
