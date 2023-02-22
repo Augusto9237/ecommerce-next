1708f56dd4aadd7cfe749e9477a80c7f9c277049
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-02-22 20:56:17 +0000

    ok

--- generate.ts
+++ generate.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://thekingtesters.saleor.cloud/",
+  schema: "https://demo.saleor.io/graphql/",
   documents: "graphql/**/*.graphql",
   generates: {
     "saleor/api.ts": {
