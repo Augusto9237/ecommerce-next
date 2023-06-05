2bc6bcc2b605f1169bd433cb88b7455be82d835f
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-06-05 16:12:40 +0000

    feat: in progress categories

--- src/components/CollapsibleCategories.tsx
+++ src/components/CollapsibleCategories.tsx
@@ -1,3 +1,4 @@
+'use client'
 import * as Collapsible from '@radix-ui/react-collapsible';
 import clsx from 'clsx';
 import { useContext } from 'react';
@@ -8,14 +9,18 @@ export function CollapsibleCategories() {
     const { sidebarState } = useContext(ContextGlobals);
     return (
         <Collapsible.Root>
-            <Collapsible.Trigger asChild className={clsx("items-center  justify-between  text-base font-medium pl-3 p-2 hover:bg-background-100", {
+            <Collapsible.Trigger className={clsx("items-center  justify-between  text-base font-medium pl-3 p-2 hover:bg-background-100", {
                 'hidden': sidebarState === false,
                 'flex': sidebarState === true
             })}>
-                <span className="">Categoria 1</span>
-                <FiChevronRight size={18} className="mt-[2px] text-detailsPrimary-100" />
+                <button className='flex flex-1 flex-row w-full'>
+                    <span>Categorias</span>
+                    <FiChevronRight size={18} className="mt-[2px] text-detailsPrimary-100" />
+                </button>
             </Collapsible.Trigger>
+
             <Collapsible.Content>
+                <span>teste</span>
             </Collapsible.Content>
         </Collapsible.Root>
     );
