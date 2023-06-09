a4f22a226b7738311682d6a75da5cbc5497653ab
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-06-09 20:31:25 +0000

    ok

--- src/components/CardCheckout.tsx
+++ src/components/CardCheckout.tsx
@@ -1,3 +1,4 @@
+import { BiPlusCircle, BiMinusCircle} from "react-icons/bi";
 export function CardCheckout({ title, urlImg, pricing }) {
     return (
         <div className="relative bg-background-100 flex flex-row rounded-md justify-center items-center overflow-hidden shadow-md object-cover">
@@ -7,7 +8,15 @@ export function CardCheckout({ title, urlImg, pricing }) {
                 <strong>{title}</strong>
                 <div className="flex flex-1 justify-between">
                     <span className="text-detailsSecondary-500 flex flex-1">R$ {pricing}</span>
-                    <input className="max-w-[20px]" type="number" value={1} />
+                    <div>
+                        <button>
+                            <BiPlusCircle/>
+                        </button>
+                        <span>1</span>
+                        <button>
+                            <BiMinusCircle/>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
