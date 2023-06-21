8f9474e9355b824f834f13a9d458ffcdd5226d04
Augusto9237 <augusto.souza8330@gmail.com>
2023-06-21 01:03:24 -0300

    ok

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,14 +1,14 @@
 'use client'
-import router from "next/router";
 import { Container } from "../../../components/Container"
 import { ProductDetail } from "../../../components/ProductDetail"
-import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useLocalStorage } from "react-use";
 import { useAddProductVariantToCartMutation } from "../../../../saleor/api";
 
 
 export default function Details({ params }: { params: { id: string } }) {
     const paramsOrigin = decodeURIComponent(params.id.replace("%3D", "="));
+    const router = useRouter();
     const [token] = useLocalStorage('token');
     const [productCart, setProductCart] = useLocalStorage('cart');
     const [addProductToCart] = useAddProductVariantToCartMutation();
@@ -18,7 +18,6 @@ export default function Details({ params }: { params: { id: string } }) {
             variables: { checkoutToken: token, variantId: paramsOrigin },
         });
         setProductCart(paramsOrigin)
-        router.push("/cart");
     };
 
     return (
@@ -28,6 +27,3 @@ export default function Details({ params }: { params: { id: string } }) {
     )
 }
 
-// function addProductToCart(arg0: { variables: { checkoutToken: any; variantId: any; }; }) {
-//     throw new Error("Function not implemented.");
-// }
