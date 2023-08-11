6e41cb9ebc4b7fa547d278a9a7edbd602e9cb6d6
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2023-08-11 19:59:40 +0000

    update

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -10,11 +10,11 @@ export function ProductList() {
 
   return (
     <section>
-      <header className="flex items-center justify-between mb-5 border-b-[1px] border-solid border-border-100">
-        <h1 className="text-2xl px-4 p-2 border-b-2 border-solid border-detailsPrimary-100 max-sm:text-xl">Produtos</h1>
-        <Link href='/' className="flex items-center gap-2 text-lg pr-4">
+      <header className="flex items-center justify-between mb-5 border-b-[1px] border-solid border-border-100 px-4 max-sm:px-1">
+        <h1 className="text-2xl  p-2 border-b-2 border-solid border-detailsPrimary-100 max-sm:text-xl">Produtos</h1>
+        <Link href='/' className="flex items-center gap-2 text-lg pr-4 max-sm:pr-2">
           <span>Todos</span>
-          <FiChevronRight className="mt-1 text-detailsPrimary-100"/>
+          <FiChevronRight className="mt-1 text-detailsPrimary-100" />
         </Link>
       </header>
       <div className="grid grid-cols-2 px-4 mb-4 md:grid-cols-5 gap-4">
